83e5262442f83b93668d9560197304481d840765
shubham chambhare <shubham961120@gmail.com>
2023-09-25 12:55:23 +0530

    first push

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p=new Product();
+      int ans =  p.product(10, 100);
+      int ans1=  p.product(10,100,150);
+    double ans2= p.product(10d,1000d);
+    System.out.print(ans+" "+ ans2+" " +ans1);
+    }
+    static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 }
